52ca9c7ab9dcdfab030c16b0952c397c9779f2ef
FelipeOG1 <feliog1234@gmail.com>
2026-05-04 13:45:37 -0600

    private rows

--- main.cc
+++ main.cc
@@ -1,29 +1,43 @@
 #include <iostream>
 #include <vector> 
 #include <cassert>
+#include <concepts>
+#include<type_traits>
+
+
 class Matrix {
 private:
-    size_t rows;
-    size_t cols;
-    std::vector<float>data; 
+    size_t _rows;
+    size_t _cols;
+    std::vector<float>_data;
     inline void check_bounds(size_t r, size_t c) { 
-        assert(r <= rows);
-        assert(c <= cols);
-    } 
-public:
+        assert(r <= _rows);
+        assert(c <= _cols);
+    }
     
-    Matrix(size_t r, size_t c) : rows(r), cols(c), data(r * c) {};
-
+public:
+    Matrix(size_t r, size_t c) : _rows(r), _cols(c), _data(r * c) {};
     float& operator()(size_t r, size_t c){
         check_bounds(r, c);  
-        return data[r * cols + c];      
+        return _data[r * _cols + c];      
     }
+    
+    
+    Matrix operator+(float scalar) {
+        for (int i=0; i < (_rows*_cols); i++) {
+            _data[i] += scalar;
+        }
+        return *(this);
+    }
+    
+    size_t rows() const { return _rows; }
+    size_t cols() const { return _cols; }
+    
+    
 };
 
 int main() {
     Matrix m(3, 3);
-    m(3, 3) = 30;
-    std::cout << m(0, 0);
     return 0;
 
 }
