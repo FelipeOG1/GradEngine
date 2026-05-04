df55e06ef0fa801636cd80cf8070790d3c516f92
FelipeOG1 <feliog1234@gmail.com>
2026-05-04 13:04:37 -0600

    init matrix

--- main.cc
+++ main.cc
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <vector> 
+class Matrix {
+private:
+    size_t rows;
+    size_t cols;
+    std::vector<float>data;   
+public:
+    
+    Matrix(size_t r, size_t c) : rows(r), cols(c), data(r * c) {};
+
+    float& operator()(size_t r, size_t c){
+          
+    }
+};
 
 int main() {
-    std::cout << "init commit" << std::endl;
+    Matrix m(2, 20);
+    
     return 0;
 
 }
