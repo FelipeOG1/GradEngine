630c7308e94702e3e19dd74f87229c3948eedbe7
FelipeOG1 <feliog1234@gmail.com>
2026-05-04 13:17:30 -0600

    inline assert

--- main.cc
+++ main.cc
@@ -1,22 +1,29 @@
 #include <iostream>
 #include <vector> 
+#include <cassert>
 class Matrix {
 private:
     size_t rows;
     size_t cols;
-    std::vector<float>data;   
+    std::vector<float>data; 
+    inline void check_bounds(size_t r, size_t c) { 
+        assert(r <= rows);
+        assert(c <= cols);
+    } 
 public:
     
     Matrix(size_t r, size_t c) : rows(r), cols(c), data(r * c) {};
 
     float& operator()(size_t r, size_t c){
-          
+        check_bounds(r, c);  
+        return data[r * cols + c];      
     }
 };
 
 int main() {
-    Matrix m(2, 20);
-    
+    Matrix m(3, 3);
+    m(3, 3) = 30;
+    std::cout << m(0, 0);
     return 0;
 
 }
