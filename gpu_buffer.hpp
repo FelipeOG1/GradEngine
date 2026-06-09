6a44b7f3bb4278dbb7916c0fc3bb6c65866ae82a
FelipeOG1 <feliog1234@gmail.com>
2026-06-09 15:42:04 -0600

    use memeset

--- gpu_buffer.hpp
+++ gpu_buffer.hpp
@@ -19,7 +19,7 @@ public:
     void upload(float* host_ptr, size_t size) { hipMemcpy(_d_ptr, host_ptr, size * sizeof(float), hipMemcpyHostToDevice); }
     void download(float* host_ptr, size_t size) { hipMemcpy(host_ptr, _d_ptr, size * sizeof(float), hipMemcpyDeviceToHost); }
     void fill(float value, size_t size) { hipMemsetD32(_d_ptr, float_as_int(value), size); }
-    void zeros(size_t size) { hipMemsetD32(_d_ptr, 0, size); }
+    void zeros(size_t size) { hipMemset(_d_ptr, 0, size); }
 	float* data() const { return _d_ptr; }
 	
     ~GpuBuffer() { hipFree(_d_ptr); }
