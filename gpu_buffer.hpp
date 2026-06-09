9d79602fc0237d436837b282de37cda74dd12a76
FelipeOG1 <feliog1234@gmail.com>
2026-06-09 14:28:53 -0600

    new fill

--- gpu_buffer.hpp
+++ gpu_buffer.hpp
@@ -2,6 +2,14 @@
 #include <hip/hip_runtime.h>
 #include <stdexcept>
 #include <vector>
+#include <cstring>
+
+inline int float_as_int(float f) {
+    int i;
+    std::memcpy(&i, &f, sizeof(float));
+    return i;
+}
+
 class GpuBuffer {
 private:
     size_t _n;
@@ -10,7 +18,7 @@ public:
     GpuBuffer(size_t n) : _n(n) { hipMalloc(&_d_ptr, _n * sizeof(float)); }
     void upload(float* host_ptr, size_t size) { hipMemcpy(_d_ptr, host_ptr, size * sizeof(float), hipMemcpyHostToDevice); }
     void download(float* host_ptr, size_t size) { hipMemcpy(host_ptr, _d_ptr, size * sizeof(float), hipMemcpyDeviceToHost); }
-    void fill(float value, size_t size) {hipMemset(_d_ptr, value, size * sizeof(float));}
+    void fill(float value, size_t size) {hipMemsetD32(_d_ptr, float_as_int(value), size); }
 	float* data() const { return _d_ptr; }
 	
     ~GpuBuffer() { hipFree(_d_ptr); }
