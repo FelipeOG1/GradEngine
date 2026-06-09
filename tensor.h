7abcc6cfbff33264a473211e09a455d6a3a254c6
FelipeOG1 <feliog1234@gmail.com>
2026-06-09 13:31:15 -0600

    accept matrix as constructor params

--- tensor.h
+++ tensor.h
@@ -15,6 +15,11 @@ private:
     std::vector<size_t> _shape;
 public:
     Tensor(size_t r, size_t c) : _data(r * c), _size( r * c ), _shape(2) { _shape[0] = r; _shape[1] = c; }
+    Tensor(std::vector<float>& matrix, size_t r, size_t c) : _data(r*c), _shape(2) {
+        _shape[0] = r; _shape[1]= c;
+        _data.upload(matrix.data(), matrix.size());
+        
+    }
     
     static Tensor rand(size_t r, size_t c);
     static Tensor randint(size_t r, size_t c, int low, int high);
