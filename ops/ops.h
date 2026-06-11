52c4513a336c55ea421523dd587a5d1233b0d5e4
FelipeOG1 <feliog1234@gmail.com>
2026-06-10 19:08:49 -0600

    move ops structs top ops.h

--- ops/ops.h
+++ ops/ops.h
@@ -3,6 +3,12 @@
 
 enum class OpCode { SUM, SUB, DIV, MUL };
 
+struct SumOp { __device__ float operator()(float a, float b) const  { return a + b; } };
+
+struct SubOp { __device__ float operator()(float a, float b) const  { return a - b; } };
+
+
+
 namespace ops {
 
     namespace elementWise {
