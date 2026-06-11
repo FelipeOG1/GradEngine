d0c40e7d2f0fdf740a151626c1e9ceeb66c74d7e
FelipeOG1 <feliog1234@gmail.com>
2026-06-10 20:21:48 -0600

    fix ops

--- ops/ops.h
+++ ops/ops.h
@@ -3,16 +3,15 @@
 
 enum class OpCode { SUM, SUB, DIV, MUL };
 
-struct SumOp { __device__ float operator()(float a, float b) const  { return a + b; } };
+struct SumOp { __device__ __forceinline__ float operator()(float a, float b) const  { return a + b; } };
 
-struct SubOp { __device__ float operator()(float a, float b) const  { return a - b; } };
-
-
+struct SubOp { __device__ __forceinline__ float operator()(float a, float b) const  { return a - b; } };
 
 namespace ops {
 
     namespace elementWise {
-
+        Tensor matsum(const Tensor& a, const Tensor& b);
+        Tensor matsub(const Tensor& a, const Tensor& b);
 
     }
 
