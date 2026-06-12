0bcfec5aac18737022f5db9ca3d39642965f3f52
FelipeOG1 <feliog1234@gmail.com>
2026-06-11 18:29:08 -0600

    rename to unary and binary ops

--- kernels/elementWise.h
+++ kernels/elementWise.h
@@ -2,6 +2,10 @@
 #include "ops/ops.h"
 
 template<typename OpType>
-__global__ void compute_element_wise(const float* A, const float* B, float* C, int w_A, OpType op);
+__global__ void compute_binary_op(const float* A, const float* B, float* C, int w_A, OpType op);
+
+template<typename OpType>
+__global__ void compute_unary_op(const float* A, float* C, int w_A, OpType op);
 
 void launch_element_wise(const float* A, const float* B, float* C, int w_A, OpCode opcode);
+void launch_element_wise(const float* A, float* C, int w_A, OpCode opcode);
