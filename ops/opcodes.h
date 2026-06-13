800f418453de343c76914fe538efd6a6e84f7d34
FelipeOG1 <feliog1234@gmail.com>
2026-06-13 00:26:46 -0600

    refactor ops

---
+++ ops/opcodes.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <hip/hip_runtime.h>
+
+enum class OpCode { SUM, SUB, DIV, MUL, RELU };
+
+struct SumOp { __device__ __forceinline__ float operator()(float a, float b) const  { return a + b; } };
+
+struct SubOp { __device__ __forceinline__ float operator()(float a, float b) const  { return a - b; } };
+
+struct MulOp { __device__ __forceinline__ float operator()(float a, float b) const  { return a * b; } };
+
+struct DivOp { __device__ __forceinline__ float operator()(float a, float b) const  { return a / b; } };
+
+struct ReluOp { __device__ __forceinline__ float operator()(float a) const { return (a > 0) ? a : 0.0f; } };
