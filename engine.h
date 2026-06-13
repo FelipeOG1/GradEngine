1102534e603f843dbc5396a4e8da4286609d86ed
FelipeOG1 <feliog1234@gmail.com>
2026-06-12 23:57:00 -0600

    singleton engine

--- engine.h
+++ engine.h
@@ -1,3 +1,21 @@
 #include "ops/ops.h"
 #include <vector>
 #include <memory>
+
+
+class AutoGradEngine {
+private:
+    AutoGradEngine() {}
+public:
+    AutoGradEngine(const AutoGradEngine&) = delete;
+    AutoGradEngine& operator=(const AutoGradEngine&) = delete;
+    
+
+    static AutoGradEngine& get_instance() {
+        static AutoGradEngine instance;
+        return instance;
+    }
+    
+
+    
+};
