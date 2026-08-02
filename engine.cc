d70a2458268265b21e7e4e78ac9906812cc84a65
FelipeOG1 <feliog1234@gmail.com>
2026-08-02 15:25:20 -0600

    clean

--- engine.cc
+++ engine.cc
@@ -14,6 +14,7 @@ namespace Engine {
 		auto new_node = std::make_shared<Node>(a_ptr, b_ptr, op);
 		c.parents_node = new_node;	
   }
+  
   void create_parents_node(const Tensor& a, Tensor& c, OpCode op) {
 		auto a_ptr = std::make_shared<Tensor>(a);
 		auto new_node = std::make_shared<Node>(a_ptr, op);
